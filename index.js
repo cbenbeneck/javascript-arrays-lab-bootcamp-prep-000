d3d24667559ec815406d2cf3b25688861d1f47dd
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-09-20 15:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Add your functions and code here
+
+var kittens = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten() {
+kittens.splice(3, 0, "Ralph");
+return kittens
+}
+
+var kittens = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten() {
+kittens.splice(0, 0, "Bob");
+return kittens
+}
+
+var kittens = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveLastKitten() {
+kittens.pop()
+return kittens 
+}
+
+var kittens = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveFirstKitten() {
+kittens.shift()
+return kittens
+}
+
+var kittens = [ "Milo", "Otis", "Garfield"]; 
+
+function appendKitten(name) {
+  var fabKittens = [...kittens]
+  fabKittens.push(name)
+  return fabKittens
+}
+
+var kittens = [ "Milo", "Otis", "Garfield"]; 
+
+function prependKitten(name) {
+  var cuteKittens = [...kittens]
+  cuteKittens.unshift(name)
+  return cuteKittens
+  }
+  
+var kittens = [ "Milo", "Otis", "Garfield"]; 
+
+function removeLastKitten() {
+  kittens.slice(0,-1)
+  return kittens
+}
+
+
+
+  
